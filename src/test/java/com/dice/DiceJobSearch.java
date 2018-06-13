33057f90850274ae18eae2f89c10a3cfe4cd26b1
sulederekoy <edanildy@gmail.com>
2018-06-13 17:37:14 -0500

    add modification

--- src/test/java/com/dice/DiceJobSearch.java
+++ src/test/java/com/dice/DiceJobSearch.java
@@ -1,5 +1,6 @@
 package com.dice;
 
+import java.time.LocalDateTime;
 import java.util.concurrent.TimeUnit;
 
 import org.openqa.selenium.By;
@@ -59,6 +60,6 @@ public class DiceJobSearch {
 		}
 		
 		driver.close();
-		
+		System.out.println("TEST COMPLETED " + LocalDateTime.now());
 	}
 }
